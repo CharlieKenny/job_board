f02e5bc632e212b444be78227baa6fc09df8b1dc
Kevin Lanzon <kevinlanzon@gmail.com>
2015-07-31 14:43:48 +0100

    adds styling and merging backend

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 var app = angular.module('jobBoardApp', ['ngRoute']);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/route1', {
+        templateUrl: 'employer.html',
+        controller: 'jobController'
+      }).
+      when('/route2', {
+        templateUrl: 'applicant.html',
+        controller: 'jobController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+}]);
